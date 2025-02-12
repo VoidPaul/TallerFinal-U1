178cdc0dfe393babbb5a3412e4eca66bd3860c5f
Paul Ramos <palvarez-2023238@kinal.edu.gt>
2025-02-12 16:18:54 -0600

    added: 'user.model' schema.

---
+++ src/user/user.model.js
@@ -0,0 +1,62 @@
+import { Schema, model } from "mongoose"
+
+const userSchema = Schema(
+  {
+    name: {
+      type: String,
+      maxLength: 25,
+      required: [true, "Name requried."],
+    },
+    lastName: {
+      type: String,
+      maxLength: 25,
+      required: [true, "Last name required."],
+    },
+    username: {
+      type: String,
+      unique: true,
+      minLength: 5,
+      maxLength: 25,
+      required: [true, "Username required."],
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "E-mail required."],
+    },
+    password: {
+      type: String,
+      required: [true, "Password required."],
+    },
+    phone: {
+      type: String,
+      minLength: 8,
+      maxLength: 10,
+    },
+    profilePicture: {
+      type: String,
+      //default: "",
+    },
+    role: {
+      type: String,
+      enum: ["ADMIN", "CUSTOMER"],
+      required: true,
+    },
+    status: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    versionKey: false,
+    timeStamps: true,
+  }
+)
+
+userSchema.methods.toJSON = function () {
+  const { password, _id, ...usuario } = this.toObject()
+  usuario.uid = _id
+  return usuario
+}
+
+export default model("User", userSchema)
